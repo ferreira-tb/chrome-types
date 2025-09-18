a2c328c7097151163726550cd7f325b9bf4cd965
Andrew Ferreira <andrew.shien2@gmail.com>
2025-09-17 23:16:02 -0300

    fix: set getter names

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -20,19 +20,19 @@ extern "C" {
   #[wasm_bindgen(thread_local_v2, js_name = chrome)]
   pub static CHROME: Chrome;
 
-  #[wasm_bindgen(method, getter)]
+  #[wasm_bindgen(method, getter = "contentSettings")]
   pub fn content_settings(this: &Chrome) -> ContentSettings;
 
-  #[wasm_bindgen(method, getter)]
+  #[wasm_bindgen(method, getter = "declarativeNetRequest")]
   pub fn declarative_net_request(this: &Chrome) -> DeclarativeNetRequest;
 
-  #[wasm_bindgen(method, getter)]
+  #[wasm_bindgen(method, getter = "runtime")]
   pub fn runtime(this: &Chrome) -> Runtime;
 
-  #[wasm_bindgen(method, getter)]
+  #[wasm_bindgen(method, getter = "storage")]
   pub fn storage(this: &Chrome) -> Storage;
 
-  #[wasm_bindgen(method, getter)]
+  #[wasm_bindgen(method, getter = "tabs")]
   pub fn tabs(this: &Chrome) -> Tabs;
 }
 
