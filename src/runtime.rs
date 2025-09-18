238cec43c050219a8a7f36c45f3b04c7c1fbe24a
Andrew Ferreira <andrew.shien2@gmail.com>
2025-09-17 22:19:27 -0300

    fix: forget closure

--- src/runtime.rs
+++ src/runtime.rs
@@ -2,9 +2,6 @@ use crate::sys::{CHROME, Chrome};
 use js_sys::{Function, Object};
 use wasm_bindgen::prelude::*;
 
-type OnMessageCallback = Closure<dyn Fn(JsValue, Object, Function) -> bool>;
-type SendResponse<'a> = &'a dyn FnOnce(&JsValue) -> Result<(), JsValue>;
-
 pub fn get_manifest() -> Object {
   CHROME.with(Chrome::runtime).get_manifest()
 }
@@ -18,19 +15,18 @@ pub async fn send_message(message: &JsValue) -> Result<JsValue, JsValue> {
 
 pub fn on_message<F>(callback: F)
 where
-  F: Fn(JsValue, Object, SendResponse) -> bool + 'static,
+  F: Fn(JsValue, Object, Function) -> bool + 'static,
 {
-  let closure = OnMessageCallback::new(move |message: JsValue, sender: Object, f: Function| {
-    callback(message, sender, &move |response: &JsValue| {
-      f.call1(&JsValue::NULL, response).map(drop)
-    })
-  });
-
+  type OnMessageCallback = Closure<dyn Fn(JsValue, Object, Function) -> bool>;
+  let closure = OnMessageCallback::new(callback);
   let closure_ref = closure.as_ref().unchecked_ref::<Function>();
+
   CHROME
     .with(Chrome::runtime)
     .on_message()
     .add_listener(closure_ref);
+
+  closure.forget();
 }
 
 pub fn on_startup(callback: &Function) {
